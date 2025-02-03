fc83bd1348dad012b2db0cfe2300479029121f5a
alexeyyakushin <alex.aka.ion@gmail.com>
2025-02-04 01:22:10 +0400

    fix для нормального разворачивания в docker с нуля

--- services/product-service/public/index.php
+++ services/product-service/public/index.php
@@ -4,6 +4,12 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+// Редирект на /api/doc, если запрошен корневой путь
+if ($_SERVER['REQUEST_URI'] === '/') {
+    header('Location: /api/doc');
+    exit();
+}
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
